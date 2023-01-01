d030ab2a2d22e1f56749e735228a2b40a50ce4a4
Ali mehr <a.mehraji75@gmail.com>
2023-01-01 10:11:23 +0330

    Complete the main script

--- main.sh
+++ main.sh
@@ -8,18 +8,19 @@ echo -e "    2. Both External & Internal Servers \n"
 
 SSH_BINARY=`which ssh`
 PS3="Enter A Number:"
+ALLOW_LOCAL_DEVICES="0.0.0.0"
 
 # Informations in servers
 info() {
-	read -p "You'r Username in External Server [Default=root]: " EX_USER
+	read -p "You'r Username in External Server [Default: root]: " EX_USER
 	EX_USER="${EX_USER:-root}"
 	#echo $EX_USER 
 
-	read -p "SSH Port For External Server [Default=22]: " EX_SSH_PORT
+	read -p "SSH Port For External Server [Default: 22]: " EX_SSH_PORT
 	EX_SSH_PORT="${EX_SSH_PORT:-22}"
 	#echo $EX_SSH_PORT
 
-	read -p "A Random Port [Default=8080]: " EX_DY_PORT
+	read -p "A Random Port [Default: 8080]: " EX_DY_PORT
 	EX_DY_PORT="${EX_DY_PORT:-8080}"
 	#echo $EX_DY_PORT
 
@@ -28,51 +29,64 @@ info() {
 		echo "ERROR: You Have To Provide an IP"
 		exit 1
 	else
-			echo $EX_IP
+			echo -e "$EX_IP\n"
 	fi
 }
 
-info
 # A function just for one external server in free land 
+_IN_Local() {
+  echo -e "\nUseage in your proxy App/Extension:"
+  echo -e "   Server: 127.0.0.1"
+  echo -e "   Port  : $EX_DY_PORT"
+}
+
+_IN_Allow() {
+  echo -e "\nUseage in your proxy App/Extension:"
+  echo -e "   Server: 127.0.0.1 in Your Host"
+  echo -e "   Server: Find Your Host Local Ip and use it in Other Devices"
+  echo -e "   Port  : $EX_DY_PORT"
+}
+
 
 external() {
 	_EX_Local() {
 		$SSH_BINARY -p $EX_SSH_PORT -CnfND $EX_DY_PORT  -l $EX_USER $EX_IP 
-	  echo -e "\nUseage in your proxy App/Extension:"
-	  echo -e "   Server: 127.0.0.1"
-	  echo -e "   Port: $EX_DY_PORT"
+		_IN_Local
 
 	}
 
 	_EX_Allow() {
-		ALLOW_LOCAL_DEVICES="0.0.0.0"
 		$SSH_BINARY -p $EX_SSH_PORT -CnfND $ALLOW_LOCAL_DEVICES:$EX_DY_PORT  -l $EX_USER $EX_IP 
+		_IN_Allow 
 
 	}
 
-	CHOICES=("Local" "Allow Other Devices" "Quit")
+	CHOICES=("Local" "Allow Other" "Quit")
 
-	select choice in CHOICES ;
+	select choice in "${CHOICES[@]}";
 	do
 		case $choice in
 			"Local")
-				_EX_Local()
+				_EX_Local
 				break
 				;;
-			"Allow Other Devices")
-				_EX_Allow()
+			"Allow Other")
+				_EX_Allow
+				break
+				;;
+			"Quit")
 				break
 				;;
 			*)
 				echo "You Need To Choose On of The Above Options."
 				exit 1
-	done 
-
-	echo -e "You Need it just in your local" 
-	echo -e "Or All Device will Use it for As A Proxy Server" 
+				;;
+		esac
+	done
 
 }
 
+info 
 external 
 
 
